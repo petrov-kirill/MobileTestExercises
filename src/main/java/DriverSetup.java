5d19f60c72a4c7fac41d36f109c02c136c18ed52
KPetrov <Kirill_Petrov@epam.com>
2018-02-10 19:16:43 +0300

    hw2 - changed locators to class, xpath;

---
+++ src/main/java/DriverSetup.java
@@ -0,0 +1,34 @@
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class DriverSetup {
+    protected AndroidDriver driver;
+//    private static final String APPLICATION_PATH =
+//            "C:\\Users\\Kirill_Petrov\\IdeaProjects\\mobile_testing_project\\src\\main\\resources\\";
+
+    private static final String APPLICATION_PATH =
+            "src\\main\\resources\\";
+
+    protected void prepareNative() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("deviceName", "emulator-5554");
+//        capabilities.setCapability("deviceName", "18b15cd77d94");
+        capabilities.setCapability("platformName", "Android");
+        File appLocation = new File(APPLICATION_PATH);
+        File app = new File(appLocation, "ContactManager.apk");
+        capabilities.setCapability("app", app.getAbsolutePath());
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+
+    protected void prepareAndroidWeb() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "emulator-5554");
+        capabilities.setCapability("browserName", "Chrome");
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+}
---
+++ src/main/java/FirstTest.java
@@ -0,0 +1,36 @@
+import org.openqa.selenium.By;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+
+public class FirstTest extends DriverSetup {
+
+    @BeforeClass(description = "preparing android native")
+    public void setup() throws MalformedURLException {
+        prepareNative();
+//        prepareAndroidWeb();
+    }
+
+    @AfterClass
+    public void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    public void SimpleEmulatorAppTest() {
+//        String appPackageName = "com.example.android.contactmanager:id/";
+//        By addBtn = By.id(appPackageName + "addContactButton");
+        By addBtn = By.xpath("//android.widget.Button[@content-desc=\"Add Contact\"]");
+//        By addBtn = By.className("android.widget.Button");
+        driver.findElement(addBtn).click();
+        System.out.println("Simple test is over");
+    }
+
+    @Test
+    public void SimpleWebEmulatorTest() {
+        driver.get("http://iana.org");
+        System.out.println("web test finished");
+    }
+}
